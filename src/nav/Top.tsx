85079ad28462a33fbfc50933a0d10be32264e4d8
VikHBoss <36492341+VikHBoss@users.noreply.github.com>
2026-03-01 17:10:35 -0500

    added logo

--- src/nav/Top.tsx
+++ src/nav/Top.tsx
@@ -1,9 +1,21 @@
 import { SettingsIcon, UserIcon } from "lucide-react";
 import Languages from "./Languages";
+import { motion } from "motion/react";
 
 export default function Top() {
   return (
     <div id="top">
+      <div>
+        <motion.div
+          whileHover={{rotate: -5}}
+          >
+        <img 
+          src="fc_logo_black.svg" 
+          alt="logo" 
+          style={{ height: "36px" }} 
+          />
+          </motion.div>
+      </div>
       <div>
         <Languages />
         <SettingsIcon className="nav-item" />
